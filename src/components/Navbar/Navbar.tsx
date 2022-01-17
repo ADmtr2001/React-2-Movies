d6ce9d19885760c564494d9ef352dca5c961a5ec
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2022-01-17 16:40:02 +0200

    refactor Navbar component code

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,15 +10,24 @@ import {MdOutlineWatchLater} from "react-icons/md";
 import logo from '../../assets/logo.png';
 import {Link} from "react-router-dom";
 
+const icons = [
+  {path: '/search', icon: BsSearch},
+  {path: '/favorite', icon: AiOutlineStar},
+  {path: '/watch-later', icon: MdOutlineWatchLater},
+  {path: '/user', icon: FaUser}
+];
+
 const Navbar = () => {
+  const listOfIcons = icons.map(icon => (
+    <NavbarIcon to={icon.path} icon={<icon.icon/>}/>
+  ))
+  ;
+
   return (
     <Wrapper>
       <div className='navbar-container'>
         <h2><Link to='/'><img src={logo} alt='logo'/></Link></h2>
-        <NavbarIcon to='/search' icon={<BsSearch/>}/>
-        <NavbarIcon to='/favorite' icon={<AiOutlineStar/>}/>
-        <NavbarIcon to='/watch-later' icon={<MdOutlineWatchLater/>}/>
-        <NavbarIcon to='/user' icon={<FaUser/>}/>
+        {listOfIcons}
       </div>
     </Wrapper>
   );
