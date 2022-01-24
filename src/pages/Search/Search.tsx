ab036cc9362ce9d1d87f899a1090b3c10fefd2ca
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2022-01-24 20:42:46 +0200

    fix bug with empty Search page on reload

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -3,23 +3,30 @@ import React, {useEffect, useRef} from 'react';
 import SearchForm from "../../components/UI/Search/SearchForm";
 import MovieSliderList from "../../components/MovieSliderList/MovieSliderList";
 
-import {useAppSelector} from "../../hooks/redux";
+import {useAppDispatch, useAppSelector} from "../../hooks/redux";
 
 import {Wrapper} from "./Search.styles";
+import MovieList from "../../components/MovieList/MovieList";
+import {fetchAsyncMovies, fetchAsyncShows} from "../../store/reducers/movie/movieActionCreators";
 
 const Search = () => {
+  const dispatch = useAppDispatch();
   const searchRef = useRef<HTMLInputElement>(null);
   const {movies, shows, moviesIsLoading, showsIsLoading} = useAppSelector(state => state.movie);
 
   useEffect(() => {
     searchRef?.current?.focus();
+
+    if (movies.length === 0) {
+      dispatch(fetchAsyncMovies('Harry Potter'));
+      dispatch(fetchAsyncShows('American'));
+    }
   }, []);
 
   return (
     <Wrapper>
       <SearchForm ref={searchRef}/>
-      {movies.length !== 0 && <MovieSliderList movies={movies} title={'Movies'} isLoading={moviesIsLoading}/>}
-      {shows.length !== 0 && <MovieSliderList movies={shows} title={'Shows'} isLoading={showsIsLoading}/>}
+      {movies.length !== 0 && <MovieList movies={[...movies, ...shows]}/>}
       {movies.length === 0 && shows.length === 0 && <h1 className='error-container'>Sorry, site search did not return any results. Try changing or shortening your request.</h1>}
     </Wrapper>
   );
