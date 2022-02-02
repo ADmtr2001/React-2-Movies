bf12f0a82738a0f34ea7d3aa20428271b3884a13
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2022-02-02 22:50:12 +0200

    add open login modal if add to favorite/later button was clicked withoout auth

--- src/components/UI/FilmActionButton/FilmActionButton.tsx
+++ src/components/UI/FilmActionButton/FilmActionButton.tsx
@@ -1,14 +1,26 @@
 import React, {FC} from 'react';
 
 import {Wrapper} from "./FilmActionButton.styles";
+import {useAppDispatch, useAppSelector} from "../../../hooks/redux";
+import {setIsLoginVisible} from "../../../store/reducers/global/globalSlice";
 
 interface FilmActionButtonProps {
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const FilmActionButton: FC<FilmActionButtonProps> = (props) => {
+const FilmActionButton: FC<FilmActionButtonProps> = ({onClick, children}) => {
+  const {user} = useAppSelector(state => state.user);
+  const dispatch = useAppDispatch();
+
+  const openModal = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    dispatch(setIsLoginVisible(true));
+  }
+
   return (
-    <Wrapper {...props}/>
+    <Wrapper onClick={user ? onClick : openModal}>{children}</Wrapper>
   );
 };
 
